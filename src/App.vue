88b94ba7d9740e5709a14d4d273f05c5c81713e1
hk <123@a.com>
2024-01-01 20:21:14 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import axios from 'axios'
+import { onBeforeMount } from 'vue';
+
+const lk = ref()
+onBeforeMount(() => {
+  axios.get('https://yesno.wtf/api').then(res => {
+    console.log(res.data)
+  })
+})
+
+</script>
+
+<template></template>
